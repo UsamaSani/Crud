e80e7494682e43e4a6d4a82caf17bb5e5b548fab
Usama Sani <usamakhanzada140@gmail.com>
2025-07-26 02:35:53 +0500

    initial

--- nestjs-backend-main/src/app.module.ts
+++ nestjs-backend-main/src/app.module.ts
@@ -7,9 +7,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://itsusamasani:UsWpOiynsFz7njew@usamasani.cu5hc8n.mongodb.net/crud',
+      '',
     ),
-    //  MongooseModule.forRoot('mongodb+srv://usaidmalik455:gTZBd11o431qhUGP@cluster0.ansuc.mongodb.net/nest-crud'),
+
     UserModule,
   ],
   controllers: [AppController],
